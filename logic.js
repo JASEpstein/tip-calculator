424a3c5282b9b23fd383a583dcb03d5e83c37513
JASEpstein <jasepstein@gmail.com>
2019-02-19 23:56:57 -0600

    Restaurant tip card MVP

--- logic.js
+++ logic.js
@@ -6,3 +6,22 @@ PSEUDOCODE:
 3. Based on which level, the tip and bill amounts are displayed at the bottom
 
 */
+
+var tipAmt = 0;
+var totalAmt = 0;
+
+
+$('.quality').on('click', function () {
+    var percentage = $(this).attr('value');
+    var inputAmt = $('#inputAmt').val();
+    tipAmt = Math.round((inputAmt * percentage) * 100) / 100;
+    totalAmt = (Math.round((inputAmt) * 100) / 100) + (Math.round((tipAmt) * 100) / 100);
+    $('#tipAmt').text('$' + tipAmt.toFixed(2));
+    $('#totalAmt').text('$' + totalAmt.toFixed(2));
+});
+
+$('#resetBtn').on('click', function () {
+    $('#tipAmt').text('$0.00');
+    $('#totalAmt').text('$0.00');
+    $('#inputAmt').val('');
+});
